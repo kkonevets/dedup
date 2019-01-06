9fc97a432bd2e6e3a60c19cf49bd48c635deef26
Kirill Konevets <kkonevets@gmail.com>
2019-01-06 17:51:06 +0300

    all

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -35,50 +35,6 @@ def save_mid2et(singles):
         pickle.dump(id2et, f)
 
 
-def sample_one(found, et, nchoices, prior, synid):
-    df = [(et['id'],
-           synid,
-           int(el['id']),
-           el['score'],
-           int(int(el['id']) == et['srcId']),
-           -1)
-          for el in found]
-    df = pd.DataFrame.from_records(df)
-    df.columns = ['qid', 'synid', 'fid', 'score', 'target', 'ix']
-    df['ix'] = df.index
-
-    if len(found) > nchoices:
-        # prior index should be sorted in ascending order[-2,-1,0,1,2,3, ...]
-        padded = np.pad(df['score'], (2, len(prior) - len(df) - 2),
-                        mode='constant', constant_values=(df['score'].mean(), 0))
-        padded /= padded.sum()
-        # now -1 and -2 have mean probability, see TODO below
-        p = np.multiply(padded, prior)
-        p /= p.sum()
-        last_ix = len(found) - 1
-        ixs = np.random.choice([last_ix, last_ix] + prior.index[2:].tolist(), nchoices + 1,
-                               replace=False, p=p)
-
-        # TODO: sample randomly from out of range
-        # out_of_range = set(ixs).intersection({-1, -2})
-        # for ix in out_of_range:
-        #     1
-    else:
-        ixs = df.index
-
-    df['score'] /= df['score'].sum()
-    samples = df.loc[ixs]
-    values = samples.values.tolist()
-    if samples['target'].max() == 0:
-        target = df[df['target'] == 1]
-        if len(target):
-            values[0] = target.iloc[0].values.tolist()
-        else:
-            values[0] = [et['id'], synid, et['srcId'], 0, 1, -1]
-
-    return values
-
-
 def get_prior(anew=False):
     prior_file = '../data/dedup/priors.csv'
     if anew:
@@ -124,6 +80,50 @@ def save_positions(positions, nrows):
     # pos_sort = positions.sort_values('i', ascending=False)
 
 
+def sample_one(found, et, nchoices, prior, synid):
+    df = [(et['id'],
+           synid,
+           int(el['id']),
+           el['score'],
+           int(int(el['id']) == et['srcId']),
+           None)
+          for el in found]
+    df = pd.DataFrame.from_records(df)
+    df.columns = ['qid', 'synid', 'fid', 'score', 'target', 'ix']
+    df['ix'] = df.index
+
+    if len(found) > nchoices:
+        # prior index should be sorted in ascending order[-2,-1,0,1,2,3, ...]
+        padded = np.pad(df['score'], (2, len(prior) - len(df) - 2),
+                        mode='constant', constant_values=(df['score'].mean(), 0))
+        padded /= padded.sum()
+        # now -1 and -2 have mean probability, see TODO below
+        p = np.multiply(padded, prior)
+        p /= p.sum()
+        last_ix = len(found) - 1
+        ixs = np.random.choice([last_ix, last_ix] + prior.index[2:].tolist(), nchoices + 1,
+                               replace=False, p=p)
+
+        # TODO: sample randomly from out of range
+        # out_of_range = set(ixs).intersection({-1, -2})
+        # for ix in out_of_range:
+        #     1
+    else:
+        ixs = df.index
+
+    df['score'] /= df['score'].sum()
+    samples = df.loc[ixs]
+    values = samples.values.tolist()
+    if samples['target'].max() == 0:
+        target = df[df['target'] == 1]
+        if len(target):
+            values[0] = target.iloc[0].values.tolist()
+        else:
+            values[0] = [et['id'], synid, et['srcId'], 0, 1, -1]
+
+    return values
+
+
 def solr_sample():
     with open('../data/1cfresh/1cfreshv4.json', 'r') as f:
         fresh = json.load(f)
@@ -142,6 +142,7 @@ def solr_sample():
 
     samples = []
     prior = get_prior(anew=False)
+    assert prior.index.isin([-1, -2]).sum() == 2
 
     for et in tqdm(singles):
         bcs = set([int(c) for c in et['barcodes']])
@@ -166,7 +167,6 @@ def solr_sample():
             found = query_solr(text, nrows)
 
             if len(found):
-                assert prior.index.isin([-1, -2]).sum() == 2
                 samples += sample_one(found, et, nchoices, prior, synid)
 
             rec = [et['id'], curname, bname]
