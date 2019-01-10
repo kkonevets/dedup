a756ade523bf5ee825736c3e847d3bcef6ff4d38
Kirill Konevets <kkonevets@gmail.com>
2019-01-10 20:52:21 +0300

    all

---
+++ preprocessing/dataset.py
@@ -0,0 +1,88 @@
+import tools
+import numpy as np
+import tensorflow as tf
+import os
+import io
+
+
+samples = tools.load_samples('../data/dedup/samples.npz')
+corpus = tools.load_samples('../data/dedup/corpus.npz')
+
+qids = samples[samples['synid'] == -1]['qid'].unique()
+sids = samples[samples['synid'] != -1]['synid'].unique()
+fids = samples['fid'].unique()
+
+
+def get_id2text(tag, ids):
+    id2text = {}
+    for _id, text in corpus[corpus[tag].isin(ids)][[tag, 'text']].values:
+        id2text[_id] = text
+    return id2text
+
+
+qid2text = get_id2text('qid', qids)
+sid2text = get_id2text('synid', sids)
+fid2text = get_id2text('fid', fids)
+
+vals = corpus[corpus['train'] != 0]['text'].values
+informative_terms = set([w for s in vals for w in s.split()])
+with io.open('../data/dedup/vocab.txt', 'w', encoding='utf8') as f:
+    for term in informative_terms:
+        f.write(term + '\n')
+
+#########################################################################
+
+
+def input_data(train=True):
+    cur_samples = samples[samples['train'] == int(train)]
+
+    labels = cur_samples['target'].values
+
+    q_terms, d_terms = [], []
+    for row in cur_samples.itertuples():
+        d_terms.append(fid2text[row.fid].split())
+        if row.synid != -1:
+            qtext = sid2text[row.synid]
+        else:
+            qtext = qid2text[row.qid]
+
+        q_terms.append(qtext.split())
+
+    # TODO: add DNN features: score, ix, brands ...
+
+    return np.array(q_terms),  np.array(d_terms), labels
+
+
+train_data = input_data(True)
+test_data = input_data(False)
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(values):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=values))
+
+
+def to_example(data, filename):
+    writer = tf.python_io.TFRecordWriter(filename)
+    q_terms, d_terms, labels = data
+    for q, d, l in zip(q_terms, d_terms, labels):
+            # Create a feature
+        feature = {
+            'q_terms': _bytes_feature([tf.compat.as_bytes(qi) for qi in q]),
+            'd_terms': _bytes_feature([tf.compat.as_bytes(di) for di in d]),
+            'labels': _int64_feature(int(l)),
+        }
+        # Create an example protocol buffer
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+
+        # Serialize to string and write on the file
+        writer.write(example.SerializeToString())
+
+    writer.close()
+
+
+to_example(train_data, '../data/dedup/train.tfrecord')
+to_example(test_data, '../data/dedup/test.tfrecord')
--- wide_deep.py
+++ wide_deep.py
@@ -1,47 +1,108 @@
-import tools
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import tools
-from tqdm import tqdm
+from collections import Counter
+import os
 import scipy
-
-samples = tools.load_samples('../data/dedup/samples.npz')
-tfidf = tools.do_unpickle('../data/dedup/tfidf_model.pkl')
-corpus = tools.load_samples('../data/dedup/corpus.npz')
-
-qids = samples[samples['synid'] == -1]['qid'].unique()
-sids = samples[samples['synid'] != -1]['synid'].unique()
-fids = samples['fid'].unique()
+from tqdm import tqdm
+import tools
+import tensorflow as tf
+import numpy as np
+import pandas as pd
 
 
-def get_id2text(tag, ids):
-    id2text = {}
-    for _id, text in corpus[corpus[tag].isin(ids)][[tag, 'text']].values:
-        id2text[_id] = text
-    return id2text
+def _parse_function(record):
+    """Extracts features and labels.
+
+    Args:
+      record: File path to a TFRecord file    
+    Returns:
+      A `tuple` `(labels, features)`:
+        features: A dict of tensors representing the features
+        labels: A tensor with the corresponding labels.
+    """
+    features = {
+        # terms are strings of varying lengths
+        "q_terms": tf.VarLenFeature(dtype=tf.string),
+        "d_terms": tf.VarLenFeature(dtype=tf.string),
+        # labels are 0 or 1
+        "labels": tf.FixedLenFeature(shape=[1], dtype=tf.int64)
+    }
+
+    parsed_features = tf.parse_single_example(record, features)
+
+    q_terms = parsed_features['q_terms'].values
+    d_terms = parsed_features['d_terms'].values
+    labels = parsed_features['labels']
+
+    return {'q_terms': q_terms, 'd_terms': d_terms}, labels
 
 
-qid2text = get_id2text('qid', qids)
-sid2text = get_id2text('synid', sids)
-fid2text = get_id2text('fid', fids)
+# Create an input_fn that parses the tf.Examples from the given files,
+# and split them into features and targets.
+def _input_fn(input_filenames, batch_size=128, num_epochs=1, shuffle=True, seed=0):
+
+    # Same code as above; create a dataset and map features and labels.
+    ds = tf.data.TFRecordDataset(input_filenames)
+    ds = ds.map(_parse_function)
+
+    if shuffle:
+        ds = ds.shuffle(10000, seed=seed)
+
+    # Our feature data is variable-length, so we pad and batch
+    # each field of the dataset structure to whatever size is necessary.
+    ds = ds.padded_batch(batch_size, ds.output_shapes)
+
+    ds = ds.repeat(num_epochs)
+
+    # Return the next batch of data.
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
 
 
-def input_fn(train=True, seed=0):
-    cur_samples = samples[samples['train'] == int(train)]
-    cur_samples = cur_samples.sample(frac=1, random_state=seed)
+#########################################################################
 
-    qtexts, ftexts = [], []
-    for row in cur_samples.itertuples():
-        ftexts.append(fid2text[row.fid])
+learning_rate = 0.1
+batch_size = 128
+display_step = 100
 
-        if row.synid != -1:
-            qtext = sid2text[row.synid]
-        else:
-            qtext = qid2text[row.qid]
+#########################################################################
 
-        qtexts.append(qtext)
+q_terms_feature_column = tf.feature_column.categorical_column_with_vocabulary_file(
+    key="q_terms", vocabulary_file='../data/dedup/vocab.txt')
+d_terms_feature_column = tf.feature_column.categorical_column_with_vocabulary_file(
+    key="d_terms", vocabulary_file='../data/dedup/vocab.txt')
+wide_columns = [q_terms_feature_column,  d_terms_feature_column]
 
-    qvecs = tfidf.transform(qtexts)
-    fvecs = tfidf.transform(ftexts)
-    data = scipy.sparse.hstack([qvecs, fvecs])
+classifier = tf.estimator.LinearClassifier(
+    feature_columns=wide_columns,
+    optimizer=tf.train.FtrlOptimizer(
+        learning_rate=learning_rate,
+        # l1_regularization_strength=10.0,
+        # l2_regularization_strength=0.0
+    ),
+    #     model_dir="./model/wide"
+)
+
+#########################################################################
+
+classifier.train(
+    input_fn=lambda: _input_fn(
+        '../data/dedup/train.tfrecord', batch_size=128, num_epochs=1),
+)
+
+#########################################################################
+
+evaluation_metrics = classifier.evaluate(
+    input_fn=lambda: _input_fn(train=True, num_epochs=1),
+    steps=1000)
+print("\nTraining set metrics:")
+for m in evaluation_metrics:
+    print(m, evaluation_metrics[m])
+print("---")
+
+evaluation_metrics = classifier.evaluate(
+    input_fn=lambda: _input_fn(train=False, num_epochs=1),
+    steps=1000)
+
+print("\nTest set metrics:")
+for m in evaluation_metrics:
+    print(m, evaluation_metrics[m])
+print("---")
