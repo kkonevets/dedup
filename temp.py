d744e27d8af35d62b2fb7c4065ba3573079bf4e1
Kirill Konevets <kkonevets@gmail.com>
2019-01-11 17:14:09 +0300

    all

--- temp.py
+++ temp.py
@@ -2,5 +2,5 @@ from itertools import islice
 
 
 q_terms,  d_terms, labels = train_data
-for q, d, s in islice(zip(q_terms,  d_terms, labels), 120, 140):
+for q, d, s in islice(zip(q_terms,  d_terms, labels), 150, 160):
     print(q, d, s)
