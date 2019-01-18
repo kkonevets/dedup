fb63ee95324ea21ef656fc02d2ef1220aec7cdab
Kirill Konevets <kkonevets@gmail.com>
2019-01-18 08:02:09 +0300

    all

---
+++ ranklib.sh
@@ -0,0 +1,4 @@
+java -jar RankLib-2.11.jar -train train_letor.txt -tvs 0.8 -test test_letor.txt -ranker 6 -metric2t P@1 -metric2T P@1 -save lmart.txt
+java -jar RankLib-2.11.jar -train train_letor.txt -tvs 0.8 -test test_letor.txt -ranker 7 -metric2T NDCG@1 -save lnet.txt
+
+java -jar RankLib-2.11.jar -load lmart.txt -rank test_letor.txt -score myScoreFile.txt
