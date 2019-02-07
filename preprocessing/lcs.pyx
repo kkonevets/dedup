22084eedccafcf93d1545395cc33e7dfa76f0d0e
Kirill Konevets <kkonevets@gmail.com>
2019-02-07 20:29:30 +0300

    all

--- preprocessing/lcs.pyx
+++ preprocessing/lcs.pyx
@@ -8,9 +8,6 @@ import numpy as np
 
 cimport cython
 cimport numpy as np
-from libcpp.string cimport string
-from libcpp.vector cimport vector
-from libcpp cimport bool
 
 cdef inline int int_max(int a, int b): return a if a >= b else b
 
@@ -29,14 +26,10 @@ def longest_common_subsequence(X, Y):
     #cdef np.ndarray[np.int32_t, ndim=2] C = np.zeros([m+1, n+1], dtype=np.int32)
     cdef np.ndarray[np.uint16_t, ndim=2] C = np.zeros([m+1, n+1], dtype=np.uint16)
 
-    # convert X, Y to C++ standard containers
-    cdef vector[string] xx = [elem.encode('utf-8') for elem in X]
-    cdef vector[string] yy = [elem.encode('utf-8') for elem in Y]
-
     cdef int i, j
     for i in range(1, m+1):
         for j in range(1, n+1):
-            if xx[i-1] == yy[j-1]:
+            if X[i-1] == Y[j-1]:
                 C[i, j] = C[i-1, j-1] + 1
             else:
                 C[i, j] = int_max(C[i, j-1], C[i-1, j])
