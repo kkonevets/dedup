365d26a4941e4619c4af2fd7262dfbafb2e11b24
Kirill Konevets <kkonevets@gmail.com>
2019-02-20 17:35:58 +0300

    all

--- pipeline.sh
+++ pipeline.sh
@@ -8,10 +8,10 @@ dst="ubuntu@10.72.102.67"
 
 export PYTHONPATH="$PYTHONPATH:$PWD"
 
-# printf "${RED}sampling\n${NC}"
-# python3 preprocessing/sampling.py \
-# --data_dir=../data/dedup/ \
-# --nrows=50
+printf "${RED}sampling\n${NC}"
+python3 preprocessing/sampling.py \
+--data_dir=../data/dedup/ \
+--nrows=50 \
 # --for_test
 
 printf "${RED}corpus\n${NC}"
@@ -20,6 +20,6 @@ python3 preprocessing/corpus.py \
 --build_tfidf
 
 printf "${RED}coping to ${NC}$dst\n"
-rsync -amvzP ../data/dedup/* $dst:/home/ubuntu/data/dedup
+rsync -lptgoDmvzP ../data/dedup/* $dst:/home/ubuntu/data/dedup
 
 ssh $dst 'bash -s' < features.sh
