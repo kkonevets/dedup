471854eac1392c24286399a2c33a50cb642d1c47
Kirill Konevets <kkonevets@gmail.com>
2019-02-22 16:53:49 +0300

    all

--- preprocessing/tokenizer.pyx
+++ preprocessing/tokenizer.pyx
@@ -1,9 +1,24 @@
 cimport cython
+from cpython cimport array
+import array
+
+cdef substitute = frozenset(u'~/-\[\]()|{}:^+')
 
 cpdef tokenize(s):
     cdef:
         unicode us = <unicode>s
-        Py_UCS4 c
-    
+        Py_UCS4 prev, c
+        unsigned int i = 0
+        array.array out = array.array('u')
+
     for c in us:
-        c
+        if c == u'ё':
+            out.append(u'е')
+        elif c in substitute:
+            out.append(' ')
+        else:
+            out.append(c)
+        
+        i += 1
+    
+    return out
