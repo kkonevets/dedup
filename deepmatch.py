89987561b102cff41d400d5398ccd351252c0250
Kirill Konevets <kkonevets@gmail.com>
2019-04-01 16:10:07 +0300

    all

--- deepmatch.py
+++ deepmatch.py
@@ -51,23 +51,12 @@ import numpy as np
 
 preds = pd.read_csv('../data/dedup/deepmatch/preds.csv')
 preds.columns = ['id', 'prob']
-
-texts = pd.read_csv('../data/dedup/deepmatch/test.csv')
-texts = texts.merge(preds, on='id')
-texts.set_index('id', inplace=True)
-
+ 
 preds.set_index('id', inplace=True)
 samples = tools.load_samples('../data/dedup/samples.npz')
 samples = samples[samples['ix']!=-1]
 test = samples[samples['train']==0]
 test = test.merge(preds, left_index=True, right_index=True)
-
-texts = texts.merge(test[['qid', 'synid', 'fid']], left_index=True, right_index=True)
-texts = texts[['qid', 'synid', 'fid', 'label', 'prob', 'left_name', 'right_name']]
-texts.sort_values(['qid', 'synid', 'label', 'fid'], 
-                  ascending=[True, True, False, True], inplace=True)
-texts = texts.astype({'qid':int, 'fid':int, 'label':int}, errors='ignore')
-texts.to_csv('../data/dedup/deepmatch/test_watch.csv', index=False)
 
 recall_scale = scoring.get_recall_test_scale()
 scoring.plot_precision_recall(test['target'], test['prob'], tag='_deep',
@@ -75,7 +64,27 @@ scoring.plot_precision_recall(test['target'], test['prob'], tag='_deep',
 scoring.topn_precision_recall_curve(test, 
                 topns=[1,2,3,5,10,20], n_thresholds=100, tag='_deep')
 
+texts = pd.read_csv('../data/dedup/deepmatch/test.csv')
+texts = texts.merge(preds, on='id')
+texts.set_index('id', inplace=True)
+ 
+texts = texts.merge(test[['qid', 'synid', 'fid']], left_index=True, right_index=True)
+texts = texts[['qid', 'synid', 'fid', 'label', 'prob', 'left_name', 'right_name']]
+texts.sort_values(['qid', 'synid', 'label', 'fid'], 
+                  ascending=[True, True, False, True], inplace=True)
+texts = texts.astype({'qid':int, 'fid':int, 'label':int}, errors='ignore')
+texts.to_csv('../data/dedup/deepmatch/test_watch.csv', index=False)
+
 t1 = texts[(texts['label']==0)&(texts['prob']>0.9)]
 t2 = texts[(texts['label']==1)&(texts['prob']<0.1)]
 t1.to_csv('../data/dedup/deepmatch/typeI.csv')
 t2.to_csv('../data/dedup/deepmatch/typeII.csv')
+
+
+prob = preds['prob']
+prob[prob>0.9].describe()
+ 
+y_true, probas_pred = test['target'], test['prob']
+cond = test['prob']>0.99
+sub=test[cond][['target', 'prob']]
+sub['target'].sum()/len(sub['prob'])
